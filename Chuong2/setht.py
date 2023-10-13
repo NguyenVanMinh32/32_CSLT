1a8084a13852bbc87d899d219815aa0d5d51a537
NguyenVanMinh32 <146708293+NguyenVanMinh32@users.noreply.github.com>
2023-10-13 10:47:59 +0700

    bt chuong

--- Chuong2/setht.py
+++
@@ -1,6 +0,0 @@
-a=45
-b=67
-c=0
-print(c=a,a=b,  c=b)
-print("ban gioi lam")
-print("sao jro ejw roie ")
--- thiNghiem.py
+++ thiNghiem.py
@@ -2,4 +2,9 @@
 '''print('Nhập tên của bạn:')
 x = input()
 print('QuanTriMang xin chào bạn, ' + x)'''
-print("Hello\nWorld\n!")
+#print("Hello\nWorld\n!")
+#len('fdutyu')
+'''a = bytes("ŽString", encoding = 'utf-8')
+s = str(a, encoding = "ascii", errors ="replace")
+print(s)'''
+print(float(25))
