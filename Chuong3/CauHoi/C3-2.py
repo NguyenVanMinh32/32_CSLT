1995355f24b3a9185c78343b8b0a715c9367dcee
NguyenVanMinh32 <146708293+NguyenVanMinh32@users.noreply.github.com>
2023-11-01 09:31:04 +0700

    et et ert

--- Chuong3/CauHoi/C3-2.py
+++ Chuong3/CauHoi/C3-2.py
@@ -2,8 +2,8 @@ n=int(input('n='))
 i=1
 while i<=n:
     if i%10 ==0:
-        print(i,end= '\n')
+        print(str(i).rjust(2),end='\n')
         i=i+1
     else :
-        print(i,end=' ')
+        print(str(i).rjust(2),end=' ')
         i=i+1
