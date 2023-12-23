2a55477b8b4aee51196133ddce73f3fff0247c0a
NguyenVanMinh32 <ng.van.minh.080905@gmail.com>
2023-12-23 07:52:43 +0700

    sdf

---
+++ Chuong7/slide/sl6.py
@@ -0,0 +1,7 @@
+# print("\"Đen Vâu\" is a rap singer\nHe\'s 32 years old\nI want to become a rapper")
+print(r""Đen Vâu" is a rap singer\nHe's 32 years old\nI want to become a rapper")
+# st='''Diibfw
+# ườniwn
+# ưtowug
+# ưenouwh'''
+# print(st)
