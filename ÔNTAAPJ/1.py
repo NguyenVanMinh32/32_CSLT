571f51c43c8bb87b06d175c31ddcd13052bca0ad
NguyenVanMinh32 <146708293+NguyenVanMinh32@users.noreply.github.com>
2024-01-20 21:03:47 +0700

    wrt

--- ÔNTAAPJ/1.py
+++ ÔNTAAPJ/1.py
@@ -1,1 +1,6 @@
 print('')
+@Các lớp CSLT kỳ 1/2023:
+Sáng mai 07h40, ngày 21/01/2024, tập trung tại phòng C201 (CSLT1), C202 (CSLT2). Chủ động chia đều chỗ ngồi, chuẩn bị máy tính, cắm sạc, đưa toàn bộ túi sách, áo khoác, điện thoại di động lên phía trên. Đúng 08h00 bắt đầu tính giờ làm bài.
+Lưu ý:
+SV thực hiện bài thi trên phần mềm MySEB, SV cần test lại phần mềm (tải bản test tại đây >> https://bom.so/LQpyX7)
+SV sử dụng macbook, cần đổi sang máy Windows để làm bài thi.
