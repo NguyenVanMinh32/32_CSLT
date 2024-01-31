d951b953512e14facf5cf52fc630bd6ee6b89ac1
NguyenVanMinh32 <146708293+NguyenVanMinh32@users.noreply.github.com>
2024-01-31 17:17:49 +0700

    sdf

--- Chuong4/BTslide/hàm số nguyên tố.py
+++ Chuong4/BTslide/hàm số nguyên tố.py
@@ -1,17 +1,17 @@
 def so_nguyen_to():
     n=int(input(''))
-def check():
-    global da
+    return n
+def check(n):
     for i in range(2,n):
         if n%i==0:
             da='SNT'
             break
     if n==1 or n==2:
         da="SNT"
-def inkq():
-    pass
-print(n,da)
-    
-so_nguyen_to()
-check()
-inkq()
+    return da,n
+def inkq(da,n):
+    print(f'{n} là {da}')
+
+n=so_nguyen_to()
+da,n=check(n)
+inkq(da,n)
